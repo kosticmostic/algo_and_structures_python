ddde0c06bee5688b03478b4e1d9720f933e8d487
KONSTANTIN <kosticmosticph@gmail.com>
2019-08-18 17:33:17 +0300

    Дз 5

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,71 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+
+import collections
+
+
+def aver_profit(named_tuple):
+    return sum(named_tuple) / len(named_tuple)
+
+
+company = collections.namedtuple('company', 'name q1 q2 q3 q4 aver')
+
+company_list = []
+
+num_company = int(input('Введите количество компаний: '))
+
+for _ in range(num_company):
+    name = input('Введите наименование компании: ')
+    quarters = [float(input(f'Введите прибыль за {i + 1} квартал: ')) for i in range(4)]
+    quarters.append(aver_profit(quarters))
+    company_list.append(company(name, *quarters))
+
+all_aver = []
+for company in company_list:
+    all_aver.append(company.aver)
+
+average_profit = aver_profit(all_aver)
+# print(average_profit)
+min = []
+max = []
+
+for company in company_list:
+    if company.aver <= average_profit:
+        min.append(company.name)
+    else:
+        max.append(company.name)
+
+print(f'\nКомпании с прибылью выше средней:\n {", ".join(max)} \n \n ')
+
+print(f'Компании с прибылью не выше средней:\n {", ".join(min)}')
+
+
+'''
+Введите количество компаний: 3
+Введите наименование компании: 1
+Введите прибыль за 1 квартал: 1
+Введите прибыль за 2 квартал: 1
+Введите прибыль за 3 квартал: 1
+Введите прибыль за 4 квартал: 1
+Введите наименование компании: 2
+Введите прибыль за 1 квартал: 2
+Введите прибыль за 2 квартал: 2
+Введите прибыль за 3 квартал: 2
+Введите прибыль за 4 квартал: 2
+Введите наименование компании: 3
+Введите прибыль за 1 квартал: 3
+Введите прибыль за 2 квартал: 3
+Введите прибыль за 3 квартал: 3
+Введите прибыль за 4 квартал: 3
+2.0
+
+Компании с прибылью выше средней:
+ 3 
+ 
+ 
+Компании с прибылью не выше средней:
+ 1, 2
+'''
+
+
--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -5,3 +5,31 @@
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+
+from collections import defaultdict
+
+nums = defaultdict(list)
+input_nums = input('Введите два числа через прбел: ').split()
+
+for num in input_nums:
+    nums[num] = list(num)
+
+
+sum = 0
+mul = 1
+
+for num in nums.keys():
+    num = int(num, 16)
+    sum += num
+    mul *= num
+
+sum_num = list(hex(sum).upper()[2:])
+mul_num = list(hex(mul).upper()[2:])
+print(*sum_num)
+print(*mul_num)
+
+
+
+
+
+
